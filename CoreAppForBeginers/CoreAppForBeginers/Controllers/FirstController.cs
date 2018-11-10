aaf3755e527a53b409e7af56c6716e9f0110b60f
faisersatti <faiser.satti@gmail.com>
2018-11-10 17:42:22 +0500

    Add all my files

---
+++ CoreAppForBeginers/CoreAppForBeginers/Controllers/FirstController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace CoreAppForBeginers.Controllers
+{
+    public class FirstController : Controller
+    {
+        private readonly IConfiguration configuration;
+        public FirstController(IConfiguration config)
+        {
+            configuration = config;
+        }
+        public IActionResult Index()
+        {
+            string con = configuration.GetConnectionString("DefaultConnection");
+            return View();
+        }
+    }
+}
---
+++ CoreAppForBeginers/CoreAppForBeginers/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CoreAppForBeginers.Models;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace CoreAppForBeginers.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration configuration;
+
+        public HomeController(IConfiguration config)
+        {
+            configuration = config;
+        }
+        public IActionResult Index()
+        {
+
+            string conn = configuration.GetConnectionString("DefaultConnection");
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
